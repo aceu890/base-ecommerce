d7cd75049d718e59dcd7d555cc55c8f445d2b23a
Fernando Andrés - Python Start <72671322+aceu890@users.noreply.github.com>
2023-01-24 06:58:43 -0300

    home page temporal

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,13 @@
 // MAIN PAGE
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import { Inter } from '@next/font/google';
+import styles from '@/styles/Home.module.css';
 import { ShopLayout } from '../components/layouts';
-import { Typography } from '@mui/material';
+import { Card, CardActionArea, CardMedia, Grid, Typography } from '@mui/material';
+import { initialData } from '../database/products';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
@@ -14,8 +15,28 @@ export default function Home() {
       <ShopLayout title={'Teslo-Shop Home'} pageDescription={'Encuentra los mejores productos de Teslo aquí'}>
         <Typography variant='h1' component='h1'>Tienda</Typography>
         <Typography variant='h2' sx={{ mb: 1 }}>Todos los productos</Typography>
+
+        <Grid container spacing={4}>
+          {
+            initialData.products.map(product => (
+              <Grid item xs={6} sm={4} key={product.slug}>
+                  <Card>
+                    <CardActionArea>
+                       <CardMedia 
+                          component='img'
+                          image={`products/${ product.images[0] }`}
+                          alt={ product.title }
+                       />
+                    </CardActionArea>
+                  </Card>
+              </Grid>
+            ))
+          }
+        </Grid>
+
+
       </ShopLayout>
-      
+
     </>
-  )
+  );
 }
