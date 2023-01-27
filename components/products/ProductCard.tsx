85047c559a0be03993f59bb1d80e8a20ff78e43b
Fernando Andrés - Python Start <72671322+aceu890@users.noreply.github.com>
2023-01-27 17:44:15 -0300

    terminar el product card

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,24 +1,54 @@
+// Página que muestra las imagenes de los productos con sus titulos
 import { IProduct } from "@/interfaces";
-import { Grid, Card, CardActionArea, CardMedia } from "@mui/material";
-import { FC } from "react";
+import { Grid, Card, CardActionArea, CardMedia, Box, Typography } from '@mui/material';
+import { FC, useMemo, useState } from "react";
 
-
+// Tipado de los productos
 interface Props {
-    product: IProduct;
+  product: IProduct;
 }
 
-export const ProductCard:FC<Props> = ({ product }) => {
+export const ProductCard: FC<Props> = ({ product }) => {
+
+  const [isHovered, setIsHovered] = useState(false);
+  
+  /* ¿Qué hace useMemo en React?
+El hook useMemo nos sirve para memorizar valores. Recibe una función que retorna el valor a memorizar y 
+como segundo parámetro opcional, un array de dependencias. Si no pasamos el array de dependencias, se ejecutará en cada renderizado.*/
+  // useMemo evita la carga de datos repetitiva, renderiza un código especifico.
+  const productImage = useMemo(() => {
+    return isHovered
+      ? `products/${product.images[1]}`
+      : `products/${product.images[0]}`
+
+// renderiza solamente las dependencias de abajo
+// si el valor de las dependencias de abajo cambian, entonces useMemo se ejecuta.
+  }, [isHovered, product.images])
+
   return (
-    <Grid item xs={6} sm={4} key={product.slug}>
+    <Grid item
+      xs={6}
+      sm={4}
+      // onMouseEnter = hover
+      onMouseEnter={ () => setIsHovered(true)}
+      onMouseLeave={ () => setIsHovered(false)}
+    >
       <Card>
         <CardActionArea>
           <CardMedia
             component="img"
-            image={`products/${product.images[0]}`}
+            className="fadeIn"
+            image={ productImage }
             alt={product.title}
+            // onLoad={ () => console.log('cargo') }
           />
         </CardActionArea>
       </Card>
+
+      <Box sx={{ mt: 1 }} className='fadeIn'>
+        <Typography fontWeight={700}>{product.title}</Typography>
+        <Typography fontWeight={500}>{`$${product.price}`}</Typography>
+      </Box>
     </Grid>
   );
 };
